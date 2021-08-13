d79c87af048cc72a960f8391524dd0be2cfc66a3
laxmisravyarudraraju <laxmisravyarudraraju@gmail.com>
2021-08-13 14:53:22 -0400

    Implemented k distinct substring with k distinct characters

---
+++ kDistinctSubstring.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class KLengthSubstring {
+    private String s;
+    
+    KLengthSubstring(String s) {
+        this.s = s;
+    }
+    
+    public Set<String> findKSubstring(int k) {
+        Set<String> answer = new HashSet<>();
+        int[] lettersCount = new int[26];
+        int low = 0;
+        int high = 0;
+        while (low <= high && high < s.length()) {
+            lettersCount[s.charAt(high) - 'a']++;
+            while (lettersCount[s.charAt(high) - 'a'] != 1) {
+                lettersCount[s.charAt(low) - 'a']--;
+                low++;
+            }
+            if (high - low + 1 == k) {
+                answer.add(s.substring(low, high + 1));
+                lettersCount[s.charAt(low) - 'a']--;
+                low++;
+            }
+            high++;
+        }
+        return answer;
+    }
+}
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        KLengthSubstring k = new KLengthSubstring("awaglknagawunagwkwagl");
+        System.out.println(k.findKSubstring(4).toString());
+    }
+}
